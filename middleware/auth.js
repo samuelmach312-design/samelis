77525dd868c51baab34c4515eac554589a17417f
samuel macharia <samuelmach.312@gmail.com>
2026-09-12 16:53:44 +0300

    SAMELIS admin only 0748440035 Till 6880156

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-﻿const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
   const token = req.header('Authorization')?.replace('Bearer ', '');
