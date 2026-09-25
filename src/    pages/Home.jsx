f240421ec90d549dfcd65840a63c5f3a3944a06a
samuelmach312-design <samuelmach.312@gmail.com>
2026-09-25 16:27:56 +0300

    Update Home.jsx

--- src/    pages/Home.jsx
+++ src/    pages/Home.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSearchParams } from "react-router-dom"
 import CategoryFilter, { categoryMap } from '../components/CategoryFilter'
 import Filters from '../components/Filters'
